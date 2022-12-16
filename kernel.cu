c60bc29bf685b23c8f86a23f8ca34cd5cc14b65e
GabrielBuckingham <81900622+GabrielBuckingham@users.noreply.github.com>
2022-12-16 15:40:40 -0800

    Update kernel.cu

--- kernel.cu
+++ kernel.cu
@@ -5,19 +5,6 @@
 
 typedef unsigned char Uint8;
 
-//__device__ class vec3 {
-//public:
-//    double x = 0.0;
-//    double y = 0.0;
-//    double z = 0.0;
-//
-//    __device__ vec3(double _x, double _y, double _z) {
-//        x = _x;
-//        y = _y;
-//        z = _z;
-//    }
-//};
-
 __global__ void kernel(Uint8* ptr, unsigned int size, unsigned int width) {
     unsigned int i = (blockIdx.x * blockDim.x) + threadIdx.x;
     if (i < size) {
